e0e637b581c2514d6d0ab036c17439bd8ef6e931
Renan Ausec <renanausec@MacBook-Air-de-Renan.local>
2024-04-10 15:51:27 -0300

    projeto finalizado

--- galeria/admin.py
+++ galeria/admin.py
@@ -6,6 +6,6 @@ class ListandoFotografias(admin.ModelAdmin):
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
     list_editable = ("publicada",)
-    list_filter = ("categoria",)
+    list_filter = ("categoria", "usuario",)
 
 admin.site.register(Fotografia, ListandoFotografias)
